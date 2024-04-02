410d624ed84e48f8ef281d2621563056e06f67c3
Mañanas <Maanas@GET-10>
2024-04-02 10:58:39 +0200

    Agregar archivos de proyecto.

---
+++ EjemploMVCcoreEntregaContinua/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EjemploMVCcoreEntregaContinua.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
